6147f3e10ac0bf83e44358d9a1adaa391612a7c8
HP-ELITEBOOK\Developer <n.nikolaevbg97@gmail.com>
2021-11-02 18:05:44 +0200

    working on course project. added CColorPoint class inhariting from CColor with x and y properties and output operator.

--- OOP/CourseProject/CourseProject/CourseProject.cpp
+++ OOP/CourseProject/CourseProject/CourseProject.cpp
@@ -31,6 +31,11 @@ public:
 		blue = this->blue;
 	}
 
+	const int RGB() const {
+		// Sums and returns the RGB numbers.
+		return this->red + this->green + this->blue;
+	}
+
 	void SetColors(const int& red, const int& green, const int& blue) {
 		// Sets RGB colors using the validation setters.
 		this->SetRed(red);
@@ -40,15 +45,15 @@ public:
 
 	ostream& Output(ostream& stream) const {
 		// Returns output info of the current color RGB.
-		return stream << "RGB: (" << this->red << ", " << this->green << ", " << this->blue << ")";
+		return stream << "RGB: (" << this->red << ", " << this->green << ", " << this->blue << ")" << endl;
 	}
 
 	friend bool operator==(const CColor& first_color, const CColor& second_color) {
-		return first_color.GetColorSum() == second_color.GetColorSum();
+		return first_color.RGB() == second_color.RGB();
 	}
 
 	friend bool operator<(const CColor& first_color, const CColor& second_color) {
-		return first_color.GetColorSum() < second_color.GetColorSum();
+		return first_color.RGB() < second_color.RGB();
 	}
 
 	friend ostream& operator<<(ostream& stream, const CColor& color) {
@@ -59,8 +64,7 @@ private:
 	// Setters.
 	void SetRed(const int& red) {
 		if (red < MIN_COLOR_NUMBER || red > MAX_COLOR_NUMBER) {
-			cout << INVALID_COLOR_NUMBER_ERROR_MESSAGE;
-			return;
+			throw INVALID_COLOR_NUMBER_ERROR_MESSAGE;
 		}
 
 		this->red = red;
@@ -68,8 +72,7 @@ private:
 
 	void SetGreen(const int& green) {
 		if (green < MIN_COLOR_NUMBER || green > MAX_COLOR_NUMBER) {
-			cout << INVALID_COLOR_NUMBER_ERROR_MESSAGE;
-			return;
+			throw INVALID_COLOR_NUMBER_ERROR_MESSAGE;
 		}
 
 		this->green = green;
@@ -77,19 +80,58 @@ private:
 
 	void SetBlue(const int& blue) {
 		if (blue < MIN_COLOR_NUMBER || blue > MAX_COLOR_NUMBER) {
-			cout << INVALID_COLOR_NUMBER_ERROR_MESSAGE;
-			return;
+			throw INVALID_COLOR_NUMBER_ERROR_MESSAGE;
 		}
 
 		this->blue = blue;
 	}
+};
 
-	int GetColorSum() const {
-		// Sums and returns the RGB numbers.
-		return this->red + this->green + this->blue;
+class CColorPoint : public CColor {
+private:
+	int x_coordinate;
+	int y_coordinate;
+
+public:
+	CColorPoint()
+		: CColor() {
+		this->SetCoordinateX(0);
+		this->SetCoordianteY(0);
+	}
+
+	CColorPoint(const int& x, const int& y, const CColor& color)
+		: CColor(color) {
+		this->SetCoordinateX(x);
+		this->SetCoordianteY(y);
+	}
+
+	ostream& Output(ostream& stream) const {
+		CColor::Output(stream);
+		return stream << "X: " << this->x_coordinate << " Y: " << this->y_coordinate << endl;
+	}
+
+	friend ostream& operator<<(ostream& stream, const CColorPoint& color_point) {
+		return color_point.Output(stream);
+	}
+
+private:
+	// Setters.
+	void SetCoordinateX(const int& x) {
+		this->x_coordinate = x;
+	}
+
+	void SetCoordianteY(const int& y) {
+		this->y_coordinate = y;
 	}
 };
 
 int main() {
+	try {
+		CColor color = CColor(16, 1, 1);
+	}
+	catch (const string error) {
+		cout << error << endl;
+	}
+
 	return 0;
 }
