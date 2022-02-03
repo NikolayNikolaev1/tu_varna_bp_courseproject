b5719667c843462bb0ed2303e63aa5454bbaa4c1
HP-ELITEBOOK\Developer <n.nikolaevbg97@gmail.com>
2022-02-03 11:05:20 +0200

    working on program systems exercises

---
+++ ProgramSystems/ExerciseApplication/ExerciseApplication/resource.h
@@ -0,0 +1,40 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ExerciseApplication.rc
+//
+#define IDC_MYICON                      2
+#define IDNO2                           8
+#define IDD_EXERCISEAPPLICATION_DIALOG  102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_EXERCISEAPPLICATION         107
+#define IDI_SMALL                       108
+#define IDC_EXERCISEAPPLICATION         109
+#define IDR_MAINFRAME                   128
+#define IDD_TASK1                       130
+#define IDD_MESSAGEBOX                  131
+#define IDD_TASK12                      132
+#define IDC_EDIT1                       1000
+#define IDMESSAGEBOX                    1001
+#define IDC_EDIT2                       1002
+#define IDC_BUTTON2                     1003
+#define IDC_BUTTON3                     1005
+#define ID_TASK1_T13                    32773
+#define ID_TASK1_T14                    32774
+#define ID_TASK1_T11                    32775
+#define ID_TASK1_T12                    32776
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
