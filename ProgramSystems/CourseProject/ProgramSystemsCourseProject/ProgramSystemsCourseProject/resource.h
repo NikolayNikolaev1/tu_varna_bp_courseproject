89056aa98e891928a6861229fa2b048e8f884d18
Nikolay Nikolaev <n.nikolaevbg97@gmail.com>
2022-09-25 19:37:04 +0300

    started course project

---
+++ ProgramSystems/CourseProject/ProgramSystemsCourseProject/ProgramSystemsCourseProject/resource.h
@@ -0,0 +1,35 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ProgramSystemsCourseProject.rc
+//
+#define IDC_MYICON                      2
+#define IDD_PROGRAMSYSTEMSCOURSEPROJECT_DIALOG 102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_PROGRAMSYSTEMSCOURSEPROJECT 107
+#define IDI_SMALL                       108
+#define IDC_PROGRAMSYSTEMSCOURSEPROJECT 109
+#define IDR_MAINFRAME                   128
+#define IDD_ARITHMETIC                  130
+#define IDCANCEL                        1001
+#define IDC_EXPRESSION                  1002
+#define IDC_CALCULATE                   1003
+#define IDC_RESULT                      1004
+#define IDC_EDIT3                       1005
+#define ID_FILE_CALCULATE               32771
+#define IDM_ARITHMETIC                  32772
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
